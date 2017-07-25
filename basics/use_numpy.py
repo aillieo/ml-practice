84cc1c1223cb41828036f240f090192f02ff1fb4
aillieo <aillieo@163.com>
2017-07-25 19:50:08 +0800

    update

--- basics/use_numpy.py
+++ basics/use_numpy.py
@@ -3,5 +3,18 @@
 
 from numpy import *
 
+def printMat():
+    array = random.rand(3,3)
+    print("arrar = %r" % array)
+    matO = mat(array)
+    print("matO = %r" % matO)
+    matI = matO.I
+    print("matI = %r" % matI)
+    matE = matO * matI
+    print("matE = %r" % matE)
+    err = eye(3) - matE
+    print("err = %r" % err)
+
+
 if __name__ == '__main__':
-    print(random.rand(4, 4))
+    printMat()
