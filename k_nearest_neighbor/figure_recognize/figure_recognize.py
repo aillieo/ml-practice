d05ba599823b4b9d957541083cfdc3426b349cf8
aillieo <aillieo@163.com>
2017-08-01 22:10:20 +0800

    finish knn figure recognition

--- k_nearest_neighbor/figure_recognize/figure_recognize.py
+++ k_nearest_neighbor/figure_recognize/figure_recognize.py
@@ -1,4 +1,50 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import numpy as np
+
+from PIL import Image
+from k_nearest_neighbor.knn import *
+import os
+
+
+def img2vec(img):
+    vec = zeros((1, 1024))
+    im = Image.open(img)
+    im.convert('1')
+    pixel = im.load()
+    for i in range(im.size[0]):
+        for j in range(im.size[1]):
+            if pixel[i, j]:
+                vec[0, 32 * j + i] = 1
+    return vec
+
+
+def classify_images():
+    image_labels = []
+    full_path = os.path.join('data', 'data_bmp', 'data4training')
+    data_training_list = os.listdir(full_path)
+    m = len(data_training_list)
+    training_mat = zeros((m, 1024))
+    for i in range(m):
+        filename_str = data_training_list[i]
+        label = int((filename_str.split('.')[0]).split('_')[0])
+        image_labels.append(label)
+        training_mat[i, :] = img2vec(os.path.join(full_path, filename_str))
+    full_path = os.path.join('data', 'data_bmp', 'data4testing')
+    data_testing_list = os.listdir(full_path)
+    error_count = 0.0
+    m_test = len(data_testing_list)
+    for i in range(m_test):
+        filename_str = data_testing_list[i]
+        label = int((filename_str.split('.')[0]).split('_')[0])
+        vector_under_test = img2vec(os.path.join(full_path, filename_str))
+        classify_result = classify0(vector_under_test, training_mat, image_labels, 3)
+        print("the classifier came back with: %d, the real answer is: %d" % (classify_result, label))
+        if classify_result != label:
+            error_count += 1.0
+    print("\nthe total number of errors is: %d" % error_count)
+    print("\nthe total error rate is: %f" % (error_count / float(m_test)))
+
+
+if __name__ == '__main__':
+    classify_images()
--- k_nearest_neighbor/knn.py
+++ k_nearest_neighbor/knn.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import numpy as np
+from numpy import *
+import operator
+
+
+def classify0(in_x, data_set, labels, k):
+    data_set_size = data_set.shape[0]
+    diff_mat = tile(in_x, (data_set_size, 1)) - data_set
+    sq_diff_mat = diff_mat ** 2
+    sq_distances = sq_diff_mat.sum(axis=1)
+    distances = sq_distances ** 0.5
+    sorted_dist_indicies = distances.argsort()
+    class_count = {}
+    for i in range(k):
+        vote_i_label = labels[sorted_dist_indicies[i]]
+        class_count[vote_i_label] = class_count.get(vote_i_label, 0) + 1
+    sorted_class_count = sorted(class_count.items(), key=operator.itemgetter(1), reverse=True)
+    return sorted_class_count[0][0]
